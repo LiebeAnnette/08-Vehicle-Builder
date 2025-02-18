c59ad5bb0556d3ad3b9392631fedd85432180680
LiebeAnnette <liebeannette@gmail.com>
2025-02-18 14:58:27 -0600

    Completed homework files and README

---
+++ 02-Challenge/Develop/src/classes/Truck.ts
@@ -0,0 +1,101 @@
+// import the Vehicle, Motorbike, Car, Wheel, and AbleToTow classes/interfaces
+import Vehicle from './Vehicle.js';
+import Motorbike from './Motorbike.js';
+import Car from './Car.js';
+import Wheel from './Wheel.js';
+import AbleToTow from '../interfaces/AbleToTow.js';
+
+
+// X TODO: The Truck class should extend the Vehicle class and should implement the AbleToTow interface
+class Truck extends Vehicle implements AbleToTow {
+  // X TODO: Declare properties of the Truck class
+  // X TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
+  // X TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+   vin: string;
+   color: string;
+   make: string;
+   model: string;
+   year: number;
+   weight: number;
+   topSpeed: number;
+   wheels: Wheel[];
+   towingCapacity: number;
+    // X TODO: Create a constructor that accepts the properties of the Truck class
+    constructor(
+      vin: string,
+      color: string,
+      make: string,
+      model: string,
+      year: number,
+      weight: number,
+      topSpeed: number,
+      wheels: Wheel[],
+      towingCapacity: number,
+    ) {
+    // X TODO: The constructor should call the constructor of the parent class, Vehicle
+      super()
+    
+    // X TODO: The constructor should initialize the properties of the Truck class
+      this.vin = vin;
+      this.color = color;
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.weight = weight;
+      this.topSpeed = topSpeed;
+      this.towingCapacity = towingCapacity;
+      // X TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+      if (wheels.length !== 4) {
+        this.wheels = [new Wheel(), new Wheel(), new Wheel(), new Wheel()];
+      } else {
+        this.wheels = wheels;
+      }
+    }
+    
+  // X TODO: Implement the tow method from the AbleToTow interface
+ 
+  tow(vehicle: Truck | Motorbike | Car): void {
+    // X TODO: Get the make and model of the vehicle if it exists
+    const make = (vehicle as any).make || "Unknown";
+    const model = (vehicle as any).model || "Unknown";
+    // X TODO: Check if the vehicle's weight is less than or equal to the truck's towing capacity
+    // X TODO: If it is, log that the vehicle is being towed
+    // X TODO: If it is not, log that the vehicle is too heavy to be towed
+    if (vehicle.weight <= this.towingCapacity) {
+      console.log(`Towing ${make} ${model}.`);
+    } else {
+      console.log(`The ${make} ${model} is too heavy to be towed.`);
+    }
+  }
+    // X TODO: Override the printDetails method from the Vehicle class
+    override printDetails(): void {
+    // X TODO: The method should call the printDetails method of the parent class
+      super.printDetails();
+    // X TODO: The method should log the details of the Truck
+    // X TODO: The details should include the VIN, make, model, year, weight, top speed, color, towing capacity, and wheels
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight} lbs`);
+    console.log(`Top Speed: ${this.topSpeed} mph`);
+    console.log(`Towing Capacity: ${this.towingCapacity} lbs`);
+
+    console.log(
+      `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
+    );
+    console.log(
+      `Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
+    );
+    }
+  }
+
+// Export the Truck class as the default export
+export default Truck;
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # 08-Vehicle-Builder
-edX Bootcamp Challenge 8 - Typescript
+edX Bootcamp Homework Challenge 8 - Typescript
+
+## Description
+
+This program prompts the user to either create a new vehicle or select an existing vehicle. After going through the creation or selection process, the user is able to perform certain actions with the selected vehicle. The user is returned to the actions menu after each action until they decide to exit the application.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Badges](#badges)
+- [Questions](#questions)
+
+## Installation
+
+git clone git @github.com:LiebeAnnette/08-Vehicle-Builder.git
+
+### Prerequisites
+Node.js
+Git
+A package manager such as npm
+
+## Usage
+
+npm install
+npm run start
+
+## Contributing
+
+My teachers, tutors, and my fellow students all contributed to helping me complete this homework assignment 😊
+
+## License
+
+MIT License
+
+## Badges
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Questions
+
+https://github.com/LiebeAnnette
+
+## Google Drive Folder with Video Link:
+
+https://drive.google.com/drive/folders/129Taa119Dmr45-DuTjeT2tc3mYZEjVza?usp=drive_link
